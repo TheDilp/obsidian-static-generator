e134c615ec166fb7dcbfc778404c85f8ce372066
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2026-06-25 13:43:17 +0200

    chore: enable subscript and superscript

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,8 @@ static MARKDOWN_PARSER_OPTIONS: LazyLock<Options> = LazyLock::new(|| {
     options.insert(Options::ENABLE_FOOTNOTES);
     options.insert(Options::ENABLE_TASKLISTS);
     options.insert(Options::ENABLE_WIKILINKS);
+    options.insert(Options::ENABLE_SUBSCRIPT);
+    options.insert(Options::ENABLE_SUPERSCRIPT);
     options.insert(Options::ENABLE_MATH);
 
     options
